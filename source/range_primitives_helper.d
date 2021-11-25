f425b11ef07e548436ba8576724b2e3ba4702554
Robert burner Schadek <robert.schadek@informatik.uni-oldenburg.de>
2021-11-25 17:49:46 +0100

    splitting the tests a bit different

--- source/range_primitives_helper.d
+++ source/range_primitives_helper.d
@@ -1,9 +1,11 @@
 module range_primitives_helper;
 
+import std.algorithm.iteration : map, filter, joiner;
+import std.algorithm.searching : any;
+import std.conv : to;
 import std.array;
-import std.range : ElementType, hasLength, isBidirectionalRange, isInfinite;
-import std.traits : ReturnType, isAutodecodableString, isAggregateType
-		, lvalueOf;
+import std.range;
+import std.traits;
 
 @safe:
 
@@ -11,25 +13,26 @@ import std.traits : ReturnType, isAutodecodableString, isAggregateType
 * This function produces a descriptive message why `R` is not an InputRange.
 * If `R` is an InputRange the returned string will say so.
 */
-string isInputRangeErrorFormatter(R)() pure nothrow {
+string isInputRangeErrorFormatter(R)() pure {
 	static if(is(typeof(R.init) == R)) {
-		bool failed = false;
-		string ret = R.stringof ~ " is not an InputRange because";
+		string ret = R.stringof ~ " is not an InputRange because:\n\t";
 
-		enum ev = isEmptyValid!R();
-		failed |= ev.failed;
-		ret ~= ev.message;
+		Test[] tests =
+				[ isValidEmpty!R()
+				, isFrontValid!R()
+				, isPopFrontValid!R()
+				];
 
-		enum fv = isFrontValid!R();
-		failed |= fv.failed;
-		ret ~= fv.message;
+		auto msg = tests
+			.filter!(t => t.failed)
+			.map!(t => t.message)
+			.joiner("\n\tand ")
+			.to!string();
 
-		enum pv = isPopFrontValid!R();
-		failed |= pv.failed;
-		ret ~= pv.message;
+		bool failed = tests.map!(t => t.failed).any();
 
 		return failed
-			? ret
+			? ret ~ msg
 			: R.stringof ~ " is an InputRange";
 	} else {
 		return R.stringof ~ " can not be tested as " ~ R.stringof ~ ".init"
@@ -41,10 +44,10 @@ string isInputRangeErrorFormatter(R)() pure nothrow {
 unittest {
 	struct Foo {}
 	enum msg = isInputRangeErrorFormatter!(Foo);
-	enum exp =`Foo is not an InputRange because
+	enum exp =`Foo is not an InputRange because:
 	the property 'empty' does not exist
-	the property 'front' does not exist
-	the function 'popFront' does not exist`;
+	and the property 'front' does not exist
+	and the function 'popFront' does not exist`;
 	static assert(msg == exp, msg ~ "\n" ~ exp);
 	string msg2 = isInputRangeErrorFormatter!(Foo);
 	assert(msg2 == exp, msg ~ "\n" ~ exp);
@@ -56,10 +59,10 @@ unittest {
 		int empty;
 	}
 	enum msg = isInputRangeErrorFormatter!(Foo);
-	enum exp =`Foo is not an InputRange because
-	the property 'empty' does not return a 'bool' but a 'int'
-	the property 'front' does not exist
-	the function 'popFront' does not exist`;
+	enum exp =`Foo is not an InputRange because:
+	the property 'empty' is not of type 'bool' but 'int'
+	and the property 'front' does not exist
+	and the function 'popFront' does not exist`;
 	static assert(msg == exp, msg ~ "\n" ~ exp);
 	string msg2 = isInputRangeErrorFormatter!(Foo);
 	assert(msg2 == exp, msg ~ "\n" ~ exp);
@@ -72,9 +75,9 @@ unittest {
 		void front();
 	}
 	enum msg = isInputRangeErrorFormatter!(Foo);
-	enum exp =`Foo is not an InputRange because
+	enum exp =`Foo is not an InputRange because:
 	the property 'front' does not return a non 'void' value
-	the function 'popFront' does not exist`;
+	and the function 'popFront' does not exist`;
 	static assert(msg == exp, msg ~ "\n" ~ exp);
 	string msg2 = isInputRangeErrorFormatter!(Foo);
 	assert(msg2 == exp, msg ~ "\n" ~ exp);
@@ -87,7 +90,7 @@ unittest {
 		int front();
 	}
 	enum msg = isInputRangeErrorFormatter!(Foo);
-	enum exp =`Foo is not an InputRange because
+	enum exp =`Foo is not an InputRange because:
 	the function 'popFront' does not exist`;
 	static assert(msg == exp, msg ~ "\n" ~ exp);
 	string msg2 = isInputRangeErrorFormatter!(Foo);
@@ -142,6 +145,7 @@ string isForwardRangeErrorFormatter(R)() pure nothrow {
 	}
 }
 
+/+
 /// ditto
 unittest {
 	struct Foo {}
@@ -248,6 +252,7 @@ unittest {
 	string msg2 = isForwardRangeErrorFormatter!(Foo);
 	assert(msg2 == exp, msg ~ "\n" ~ exp);
 }
++/
 
 /**
 * This function produces a descriptive message why `R` is not an BidirectionalRange.
@@ -283,6 +288,7 @@ string isBidirectionalRangeErrorFormatter(R)() pure nothrow {
 	}
 }
 
+/+
 /// ditto
 unittest {
 	struct Foo {}
@@ -359,6 +365,7 @@ unittest {
 	string msg2 = isBidirectionalRangeErrorFormatter!(Foo);
 	assert(msg2 == exp, "\n" ~ msg ~ "\n" ~ exp);
 }
++/
 
 /**
 * This function produces a descriptive message why `R` is not an RandomAccessRange.
@@ -440,6 +447,7 @@ string isRandomAccessRangeErrorFormatter(R)() pure nothrow {
 	}
 }
 
+/+
 /// ditto
 unittest {
 	struct Foo {}
@@ -611,6 +619,7 @@ unittest {
 	string msg2 = isRandomAccessRangeErrorFormatter!(Foo);
 	assert(msg2 == exp, "\n" ~ msg ~ "\n" ~ exp);
 }
++/
 
 private:
 
@@ -619,23 +628,9 @@ struct Test {
 	string message;
 }
 
-Test isEmptyValid(R)() {
-	enum hasEmpty = __traits(hasMember, R, "empty");
-	static if(hasEmpty) {
-		alias RTempty = ReturnType!((R r) => r.empty);
-		enum hasEmptyIsBool = is(RTempty == bool);
-		static if(!hasEmptyIsBool) {
-			return Test(true,
-			"\n\tthe property 'empty' does not return a 'bool' but "
-				~ "a '" ~ RTempty.stringof ~ "'");
-		} else {
-			return Test(false, "");
-		}
-	} else {
-		return Test(true, "\n\tthe property 'empty' does not exist");
-	}
-}
-
+//
+// isFrontValid
+//
 Test isFrontValid(R)() {
 	enum hasFront = is(typeof((return ref R r) => r.front));
 	static if(hasFront) {
@@ -643,16 +638,36 @@ Test isFrontValid(R)() {
 		enum hasFrontIsVoid = is(RTfront == void);
 		static if(hasFrontIsVoid) {
 			return Test(true
-				, "\n\tthe property 'front' does not return a non 'void' "
+				, "the property 'front' does not return a non 'void' "
 				~ "value");
 		} else {
 			return Test(false, "");
 		}
 	} else {
-		return Test(true, "\n\tthe property 'front' does not exist");
+		return Test(true, "the property 'front' does not exist");
 	}
 }
 
+unittest {
+	struct Foo{}
+	enum Test a = isFrontValid!Foo();
+	static assert(a.failed);
+	static assert(a.message == "the property 'front' does not exist"
+			, a.message);
+}
+
+unittest {
+	struct Foo{
+		int front;
+	}
+	enum Test a = isFrontValid!Foo();
+	static assert(!a.failed);
+	static assert(a.message.empty, a.message);
+}
+
+//
+// isBackValid
+//
 Test isBackValid(R)() {
 	enum hasBack = is(typeof((return ref R r) => r.back));
 	static if(hasBack) {
@@ -660,26 +675,88 @@ Test isBackValid(R)() {
 		enum hasBackIsEqualToFront = is(RTback == ElementType!R);
 		static if(!hasBackIsEqualToFront) {
 			return Test(true
-				, "\n\tthe property 'back' does return a '" ~ RTback.stringof
+				, "the property 'back' does return a '" ~ RTback.stringof
 				~ "' which is not equal to the type of 'front' which is '"
 				~ ElementType!(R).stringof ~ "'");
 		} else {
 			return Test(false, "");
 		}
 	} else {
-		return Test(true, "\n\tthe property 'back' does not exist");
+		return Test(true, "the property 'back' does not exist");
 	}
 }
 
+unittest {
+	struct Foo{}
+	enum Test a = isBackValid!Foo();
+	static assert(a.failed);
+	static assert(a.message == "the property 'back' does not exist"
+			, a.message);
+}
+
+unittest {
+	struct Foo{
+		int back;
+	}
+	enum Test a = isBackValid!Foo();
+	static assert(a.failed);
+	static assert(a.message == "the property 'back' does return a 'int' which is not equal to the type of 'front' which is 'void'"
+			, a.message);
+}
+
+unittest {
+	struct Foo{
+		int back;
+		double front;
+	}
+	enum Test a = isBackValid!Foo();
+	static assert(a.failed);
+	static assert(a.message == "the property 'back' does return a 'int' which is not equal to the type of 'front' which is 'double'"
+			, a.message);
+}
+
+unittest {
+	struct Foo{
+		int back;
+		int front;
+	}
+	enum Test a = isBackValid!Foo();
+	static assert(!a.failed);
+	static assert(a.message.empty, a.message);
+}
+
+//
+// isPopFrontValid
+//
 Test isPopFrontValid(R)() {
 	enum hasPopFront = is(typeof((R r) => r.popFront));
 	static if(!hasPopFront) {
-		return Test(true, "\n\tthe function 'popFront' does not exist");
+		return Test(true, "the function 'popFront' does not exist");
 	} else {
 		return Test(false, "");
 	}
 }
 
+unittest {
+	struct Foo{}
+	enum Test a = isPopFrontValid!Foo();
+	static assert(a.failed);
+	static assert(a.message == "the function 'popFront' does not exist"
+			, a.message);
+}
+
+unittest {
+	struct Foo{
+		void popFront();
+	}
+	enum Test a = isPopFrontValid!Foo();
+	static assert(!a.failed);
+	static assert(a.message.empty);
+}
+
+//
+// isSaveValid
+//
 Test isSaveValid(R)() {
 	enum hasSave = is(typeof((R r) => r.save));
 	static if(hasSave) {
@@ -687,29 +764,256 @@ Test isSaveValid(R)() {
 		enum isSaveR = is(RTsave == R);
 		static if(!isSaveR) {
 			return Test(true,
-			"\n\tthe property 'save' does not return a '" ~ R.stringof
+			"the property 'save' does not return a '" ~ R.stringof
 				~ "' but a '" ~ RTsave.stringof ~ "'");
 		} else {
 			return Test(false, "");
 		}
 	} else {
-		return Test(true, "\n\tthe property 'save' does not exist");
+		return Test(true, "the property 'save' does not exist");
 	}
 }
 
-Test isValidBoolEnum(R)() {
+unittest {
+	struct Foo{}
+	enum Test a = isSaveValid!Foo();
+	static assert(a.failed);
+	static assert(a.message == "the property 'save' does not exist");
+}
+
+unittest {
+	struct Foo{
+		int save;
+	}
+	enum Test a = isSaveValid!Foo();
+	static assert(a.failed);
+	static assert(a.message == "the property 'save' does not return a 'Foo'"
+			~ " but a 'int'");
+}
+
+//
+// isValidEmpty
+//
+Test isValidEmpty(R)() {
 	enum hasEmpty = __traits(hasMember, R, "empty");
 	static if(hasEmpty) {
-		static if (isInputRange!R && __traits(compiles, { enum e = R.empty; })) {
+		static if (__traits(compiles, { enum e = R.empty; })) {
 			enum bool b = R.empty;
 			static if(b) {
-				return Test(R.empty, "");
+				return Test(R.empty == false, "");
 			} else {
-				return Test(true, "The enum " ~ R.stringof ~ ".empty must not "
-						~ "false");
+				return Test(true, "the enum 'empty' must not be 'false'");
 			}
+		} else if(is(typeof(__traits(getMember, R, "empty")) == bool)) {
+			return Test(false, "");
 		} else {
-			return Test(true, R.stringof ~ ".empty is not an enum of type bool");
+			return Test(true, "the property 'empty' is not of type 'bool' but '"
+					~ typeof(R.empty).stringof ~ "'");
 		}
+	} else {
+		return Test(true, "the property 'empty' does not exist");
 	}
+}
+
+unittest {
+	struct Foo {}
+	enum Test a = isValidEmpty!Foo();
+	static assert(a.failed);
+	static assert(a.message == "the property 'empty' does not exist"
+			, a.message);
+}
+
+unittest {
+	struct Foo {
+		int empty;
+	}
+	enum Test a = isValidEmpty!Foo();
+	static assert(a.failed);
+	static assert(a.message ==
+			"the property 'empty' is not of type 'bool' but 'int'"
+			, a.message);
+}
+
+unittest {
+	struct Foo {
+		enum bool empty = false;
+	}
+	enum Test a = isValidEmpty!Foo();
+	static assert(a.failed);
+	static assert(a.message == "the enum 'empty' must not be 'false'"
+			, a.message);
+}
+
+unittest {
+	struct Foo {
+		bool empty;
+	}
+	enum Test a = isValidEmpty!Foo();
+	static assert(!a.failed);
+	static assert(a.message.empty);
+}
+
+unittest {
+	struct Foo {
+		enum bool empty = true;
+	}
+	enum Test a = isValidEmpty!Foo();
+	static assert(!a.failed, a.message);
+	static assert(a.message.empty);
+}
+
+//
+// isValidBoolEnum
+//
+Test isValidBoolEnum(R)() {
+	static if (__traits(compiles, { enum e = R.empty; })) {
+		enum bool b = R.empty;
+		static if(b) {
+			return Test(R.empty == false, "");
+		} else {
+			return Test(true, "the enum 'empty = true' must not be 'false'");
+		}
+	} else {
+		return Test(true, "the enum 'empty = true' does not exist");
+	}
+}
+
+unittest {
+	struct Foo {}
+	enum Test a = isValidBoolEnum!Foo();
+	static assert(a.failed);
+	static assert(a.message == "the enum 'empty = true' does not exist"
+			, a.message);
+}
+
+unittest {
+	struct Foo {
+		int empty;
+	}
+	enum Test a = isValidBoolEnum!Foo();
+	static assert(a.failed);
+	static assert(a.message == "the enum 'empty = true' does not exist"
+			, a.message);
+}
+
+unittest {
+	struct Foo {
+		enum bool empty = false;
+	}
+	enum Test a = isValidBoolEnum!Foo();
+	static assert(a.failed);
+	static assert(a.message == "the enum 'empty = true' must not be 'false'"
+			, a.message);
+}
+
+unittest {
+	struct Foo {
+		bool empty;
+	}
+	enum Test a = isValidBoolEnum!Foo();
+	static assert(a.failed);
+	static assert(a.message == "the enum 'empty = true' does not exist"
+			, a.message);
+}
+
+unittest {
+	struct Foo {
+		enum bool empty = true;
+	}
+	enum Test a = isValidBoolEnum!Foo();
+	static assert(!a.failed, a.message);
+	static assert(a.message.empty);
+}
+
+//
+// isValidOpIndex
+//
+Test isValidOpIndex(R)() {
+	static if(!is(typeof(lvalueOf!R[1]))) {
+		return Test(true, "[] aka. opIndex not possible");
+	} else static if(is(typeof(lvalueOf!R[1]))
+			&& !is(typeof(lvalueOf!R[1]) == ElementType!R))
+	{
+		return Test(true
+			, R.stringof ~ "[idx] is of type '"
+			~ typeof(lvalueOf!R[1]).stringof ~ "' but must be equal to '"
+			~ R.stringof ~ ".front' which is '" ~ ElementType!(R).stringof
+			~ "'");
+	} else {
+		return Test(false, "");
+	}
+}
+
+unittest {
+	struct Foo {}
+	enum Test a = isValidOpIndex!Foo();
+	static assert(a.failed);
+	static assert(a.message == "[] aka. opIndex not possible", a.message);
+}
+
+unittest {
+	struct Foo {
+		int opIndex(size_t i) { assert(false); }
+	}
+	enum Test a = isValidOpIndex!Foo();
+	static assert(a.failed);
+	static assert(a.message == "Foo[idx] is of type 'int' but must be equal to 'Foo.front' which is 'void'");
+}
+
+unittest {
+	struct Foo {
+		double front;
+		int opIndex(size_t i) { assert(false); }
+	}
+	enum Test a = isValidOpIndex!Foo();
+	static assert(a.failed);
+	static assert(a.message == "Foo[idx] is of type 'int' but must be equal to 'Foo.front' which is 'double'");
+}
+
+unittest {
+	struct Foo {
+		int front;
+		int opIndex(size_t i) { assert(false); }
+	}
+	enum Test a = isValidOpIndex!Foo();
+	static assert(!a.failed);
+	static assert(a.message);
+}
+
+//
+// hasValidLength
+//
+Test hasValidLength(R)() {
+	static if(hasLength!R) {
+		return Test(false, "");
+	} else {
+		return Test(true, "property 'size_t length' is not defined");
+	}
+}
+
+unittest {
+	struct Foo {}
+	enum Test a = hasValidLength!Foo();
+	static assert(a.failed);
+	static assert(a.message == "property 'size_t length' is not defined"
+			, a.message);
+}
+
+unittest {
+	struct Foo {
+		double length;
+	}
+	enum Test a = hasValidLength!Foo();
+	static assert(a.failed);
+	static assert(a.message == "property 'size_t length' is not defined"
+			, a.message);
+}
+
+unittest {
+	struct Foo {
+		size_t length;
+	}
+	enum Test a = hasValidLength!Foo();
+	static assert(!a.failed);
+	static assert(a.message.empty);
 }
